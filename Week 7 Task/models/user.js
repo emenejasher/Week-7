665caf3539fb8780a65f6b56cb59a08823abf9ae
Emmanuel Josiah <85228406+emenejasher@users.noreply.github.com>
2021-07-21 12:31:56 +0100

    Add files via upload

---
+++ Week 7 Task/models/user.js
@@ -0,0 +1,34 @@
+let users = [
+    { 
+        id : 1, 
+        fullName : "Gojo Satorou",
+        email : "dripordrown@gmail.com",
+        password : 23455546322,
+    },
+    
+    {
+        id : 2,
+        fullName : "Shinra Kusabe",
+        email: "devilfoot@gmail.com",
+        password : 23453564638,
+    },
+    {
+        id : 3,
+        fullName : "Monkey D. Luffy",
+        email :"rubberman2@gmail.com",
+        password : 23451324343,
+    },
+    {
+        id : 4,
+        fullName : "Zoro Hunter",
+        email : "piratehunter@gmail.com",
+        password : 23454225663,
+    },
+    {
+        id : 5,
+        fullName : "Sanji Vinsmoke",
+        email: "smoker4life@gmail.com",
+        password : 23451342145,
+    }]
+    
+    module.exports = users;
