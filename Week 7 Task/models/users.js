fa7bf87921aa62ac0de43c86dacfca4412774212
Emmanuel Josiah <85228406+emenejasher@users.noreply.github.com>
2021-07-21 22:32:20 +0100

    Add files via upload

---
+++ Week 7 Task/models/users.js
@@ -0,0 +1,41 @@
+let users = [
+    { 
+        id: 1, 
+        fullName: "Matthew John",
+        email: "johnmatthew@gmail.com",
+        password: 12345678901,
+    },
+    
+    {
+        id: 2,
+        fullName: "Brenda Laz",
+        email: "lazzybrenda@gmail.com",
+        password: 23456789012,
+    },
+    {
+        id: 3,
+        fullName: "Senator Paul",
+        email:"paulhunter@gmail.com",
+        password: 34567890123,
+    },
+    {
+        id: 4,
+        fullName: "Patience Christopher",
+        email: "patientone@gmail.com",
+        password: 45678901234,
+    },
+    {
+        id: 5,
+        fullName: "Latisha Wood",
+        email:"samantha@gmail.com",
+        password: 56789012345,
+    },
+    {
+        id: 6,
+        fullName: "Patrice Evra",
+        email: "evrasantacus@gmail.com",
+        password: 67890123456,
+    }
+    }]
+    
+    module.exports = users;
